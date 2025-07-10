804f75ac62399e2fd704268aeaaa2ce4e541966b
BitterKanegul <15769643+BitterKanegul@users.noreply.github.com>
2025-07-10 13:31:59 -0400

    Chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+// Learn me some good Rust
+// Gotta figure out how to get into *hygenic macros*
+//
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number:");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("rip");
+    println!("you guessed: {guess}")
 }
