3a3cdde3b083564e0c5e4474b9ab6700a9136b42
BitterKanegul <15769643+BitterKanegul@users.noreply.github.com>
2025-07-10 14:16:11 -0400

    Chapter2 complete

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,21 @@ use std::io;
 fn main() {
     println!("Guess the number:");
     let secret_number = rand::thread_rng().gen_range(1..=50);
-    println!("The secret number is {secret_number}");
+    // println!("The secret number is {secret_number}");
     loop {
         let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect("rip");
         println!("you guessed: {guess}");
-        let guess: u32 = guess.trim().parse().expect("pls enter number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too Small!"),
-            Ordering::Equal => println!("You Win!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
             Ordering::Greater => println!("Too Big"),
         }
     }
